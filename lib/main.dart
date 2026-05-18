e06ddfd2214f58cb697e37c1f122dc6a884cca27
Daniel Büttner <buda1037@h-ka.de>
2026-05-18 23:30:41 +0200

    feat: implement splash screen prototype

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:leaddesk/splash_screen_prototype.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: SplashScreenPrototype(),
     );
   }
 }
---
+++ lib/splash_screen_prototype.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class SplashScreenPrototype extends StatelessWidget {
+  const SplashScreenPrototype({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.add,
+              size: 36,
+              color: Color(0xFF7C3AED),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'LeadDesk',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 6),
+            Text(
+              'One App. Every Tradeshow.',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+                color: Colors.grey,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
